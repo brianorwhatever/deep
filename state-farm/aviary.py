bcfdfe928dfcb02fd3a092d7ca381b93bb3b91de
Ubuntu <ubuntu@ip-172-31-36-2.us-west-2.compute.internal>
2016-11-25 09:50:11 +0000

    latest work on state farm

--- state-farm/aviary.py
+++ state-farm/aviary.py
@@ -1,3 +1,25 @@
+from theano.sandbox import cuda
+
+#path = "data/state-farm/"
+path = "../nbs/data/state-farm/sample/"
+import utils; reload(utils)
+from utils import *
+from IPython.display import FileLink
+
+batch_size=64
+
+batches = get_batches(path+'train', batch_size=batch_size)
+val_batches = get_batches(path+'valid', batch_size=batch_size*2, shuffle=False)
+
+(val_classes, trn_classes, val_labels, trn_labels, val_filenames, filenames, test_filename) = get_classes(path)
+
+gen_t = image.ImageDataGenerator(rotation_range=15, height_shift_range=0.05, 
+                                 shear_range=0.1, channel_shift_range=20, width_shift_range=0.1)
+batches = get_batches(path+'train', gen_t, batch_size=batch_size)
+
+val = load_array(path+'results/val.dat')
+trn = load_array(path+'results/trn.dat')
+
 def conv1(batches):
     model = Sequential([
             BatchNormalization(axis=1, input_shape=(3,224,224)),
@@ -14,17 +36,20 @@ def conv1(batches):
             Dense(10, activation='softmax')
         ])
 
-    model.compile(Adam(lr=1e-5), loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches,
+    model.compile(Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit_generator(batches, batches.nb_sample, nb_epoch=4, validation_data=val_batches, 
                      nb_val_samples=val_batches.nb_sample)
     model.optimizer.lr = 0.001
-    model.fit_generator(batches, batches.nb_sample, nb_epoch=8, validation_data=val_batches,
+    model.fit_generator(batches, batches.nb_sample, nb_epoch=8, validation_data=val_batches, 
                      nb_val_samples=val_batches.nb_sample)
     model.optimizer.lr = 0.01
-    model.fit_generator(batches, batches.nb_sample, nb_epoch=8, validation_data=val_batches,
+    model.fit_generator(batches, batches.nb_sample, nb_epoch=12, validation_data=val_batches, 
                      nb_val_samples=val_batches.nb_sample)
     model.optimizer.lr = 0.1
-    model.fit_generator(batches, batches.nb_sample, nb_epoch=20, validation_data=val_batches,
+    model.fit_generator(batches, batches.nb_sample, nb_epoch=16, validation_data=val_batches, 
+                         nb_val_samples=val_batches.nb_sample)
+    model.optimizer.lr = 1
+    model.fit_generator(batches, batches.nb_sample, nb_epoch=16, validation_data=val_batches, 
                          nb_val_samples=val_batches.nb_sample)
     return model
 
@@ -33,3 +58,5 @@ gen_t = image.ImageDataGenerator(rotation_range=15, height_shift_range=0.05,
 batches = get_batches(path+'train', gen_t, batch_size=batch_size)
 
 model = conv1(batches)
+
+model.save_weights(path+'results/aviary_008.h5')
